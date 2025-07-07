8ac3c8379aa2720d6821ad3391bb55b921303c5f
zafer turan <132119193+Zaferturan@users.noreply.github.com>
2025-07-07 18:33:54 +0300

    Update anythingllm_api_key.ts

--- src/config/anythingllm_api_key.ts
+++ src/config/anythingllm_api_key.ts
@@ -1,2 +1,1 @@
-export const ANYTHINGLLM_API_KEY = "1J788K8-XSY4NSM-J0MHF5A-BSJMW32";
-export const ANYTHINGLLM_API_URL = "https://icdenetimapi.niluferyapayzeka.tr/api/v1/workspace/ic-denetim-robotu/chat"; 
+
